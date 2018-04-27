090e832f4948c7056f1fa503a6609905d8030439
pomfy113 <pomfy113@gmail.com>
2018-04-26 18:38:01 -0700

    Updated css and stretch

--- psswrdz-app/src/Password.js
+++ psswrdz-app/src/Password.js
@@ -4,22 +4,59 @@ import './Password.css'
 class Password extends Component {
     constructor(props) {
         super(props)
+        this.alpha = this.allAlpha();
+        this.num = this.allNum();
+        this.all = this.allChars();
+
+
         this.state = {
             description: 'Description for password',
-            password: 'p@ssw0rd'
+            password: 'p@ssw0rd',
+            hypen: false,
+            available: this.alpha
         }
-        this.availableCharacters()
     }
 
-    availableCharacters() {
-        let allChars = [];
-
-        for(let i = 33; i < 127; i++) {
-            allChars.push(String.fromCharCode(i));
+    changeAvailable(type){
+        switch(type){
+            case 'alpha':
+                this.setState({ available: this.alpha });
+                break;
+            case 'alphanum':
+                console.log(this.num)
+                console.log(this.alpha.concat(this.num))
+                this.setState({ available: this.alpha.concat(this.num) });
+                break;
+            case 'all':
+                this.setState({ available: this.all});
+                break;
         }
+    }
 
-        this.chars = allChars
-        this.charLen = allChars.length
+    allAlpha(){
+        let chars = [];
+        for(let i = 65; i < 123; i++) {
+            if(i < 91 || i > 96){
+                chars.push(String.fromCharCode(i));
+            }
+        }
+        return chars
+    }
+
+    allNum(){
+        let chars = [];
+        for(let i = 48; i < 57; i++) {
+            chars.push(String.fromCharCode(i));
+        }
+        return chars;
+    }
+
+    allChars(){
+        let chars = [];
+        for(let i = 33; i < 127; i++) {
+            chars.push(String.fromCharCode(i));
+        }
+        return chars;
     }
 
     random(n) {
@@ -28,11 +65,13 @@ class Password extends Component {
 
     generatePassword() {
         // generate a password here
-        let newPass = []
+        let newPass = "";
+        let length = this.state.available.length;
         for(let i = 0; i < 9; i++){
-            newPass.push(this.chars[this.random(this.charLen)])
-            if((i+1) % 3 == 0 && i != 8){
-                newPass.push('-')
+            newPass += this.state.available[this.random(length)]
+
+            if(this.state.hypen && (i+1) % 3 === 0 && i !== 8){
+                newPass += '-'
             }
         }
 
@@ -45,18 +84,41 @@ class Password extends Component {
                 <div>
                     <div className="password-inputs">
                         <input
+                          type="text"
                           onChange={(e) => {this.setState({ description: e.target.value })}}
                           value={this.state.description}
                         />
                       <input
+                        type="text"
                         onChange={(e) => {this.setState({ password: e.target.value })}}
                         value={this.state.password}
                       />
+                      <div className="password-checkbox">
+                          <span>
+                              <input
+                                type='checkbox'
+                                onChange={(e) => {this.setState({ hypen: !this.state.hypen })}}
+                                checked={this.state.hypen}
+                              />
+                              <label>Insert hypens</label>
+                          </span>
+
+                          <select
+                              onChange={(e) => {
+                                  this.changeAvailable(e.target.value);
+                              }}>
+                              <option value="alpha">Alphabet</option>
+                              <option value="alphanum">Alphabet + Numbers</option>
+                              <option value="all">Alphabet + Numbers + Symbols</option>
+                          </select>
+                      </div>
+                      {this.state.type}
                     </div>
-                    <button onClick={(e) => { this.generatePassword() }}>
+
+                    <button id="generate" onClick={(e) => { this.generatePassword() }}>
                         Generate Random Password
                     </button>
-                    <div><h1>{this.state.password}</h1></div>
+                    <div>{this.state.description} - {this.state.password}</div>
 
                 </div>
             </div>
